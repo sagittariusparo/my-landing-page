9b11d54628aafc08f5ff581b497bf6914fd881b1
sagittariusparo <parammita.sagittarius@gmail.com>
2025-09-02 00:41:01 +0530

    modify header with actual button links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,29 @@
-import './Header.css';
+import { Link } from "react-router-dom";
+import "./Header.css";
 
-const Header = () => {
+const Header = ({ isLoggedIn, onLogout }) => {
   return (
     <header className="header">
       <div className="menu">
         <a href="#pricing">Pricing</a>
         <a href="#about">About Us</a>
       </div>
+
       <div className="auth-buttons">
-        <button className="signin">Sign In</button>
-        <button className="register">Register</button>
+        {!isLoggedIn ? (
+          <>
+            <Link to="/signin" className="signin">
+              Sign In
+            </Link>
+            <Link to="/register" className="register">
+              Register
+            </Link>
+          </>
+        ) : (
+          <button className="logout" onClick={onLogout}>
+            Logout
+          </button>
+        )}
       </div>
     </header>
   );
